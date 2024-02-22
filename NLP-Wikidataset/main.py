2eacfc8f18662075cb32b6f7f94eafb2c1ed9166
MeinChef <yapa.nilop@gmail.com>
2024-02-22 11:30:38 +0100

    found out how to read one example, tokenisation still not working

--- NLP-Wikidataset/main.py
+++ NLP-Wikidataset/main.py
@@ -1,17 +1,35 @@
 from imports import tensorflow as tf
 from imports import tensorflow_datasets as tfds
 
+tf.config.run_functions_eagerly(True)
+tf.data.experimental.enable_debug_mode()
+
 def load_and_prep_dataset(batch_size):
     
-    train, test = tfds.load('wikipedia', split = ['train', 'test'], shuffle_files = True)
+    text = tfds.load('wikipedia')
 
-    def tokenise():
-        pass
+    # for elem in text['train']:
+    #     print(elem)
+    #     break
+    # return
+
     
-    train = train.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
-    test  =  test.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    tokenise = tf.keras.layers.TextVectorization()
+
+    test = ['word', 'foo', 'bar', 'taz']
+    for elem in text['train']:
+        tokenise(elem['text'])
+    
+    print(tokenise.get_vocabulary())
+    
+
+    #train = train.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    #test  =  test.cache().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+    return 
 
 # do we want to get chars or rather words?????
+    # I concluded this is uneccesary.
 def id_from_chr(char):
     return tf.keras.layers.StringLookup(vocabulary = list(vocab), mask_token = None)(char)
 
@@ -21,11 +39,13 @@ def char_from_id(id):
 def text_from_ids(ids):
     return tf.strings.reduce_join(char_from_id(ids), axis = -1)
 
+# rather, we want to train a model on "vocabulary"
+# see tokenise
+
 if __name__ == '__main__':
 
     BATCH_SIZE = 256
 
     global vocab 
-    vocab = set('a', 'b', 'c', 'd')
 
-    train, test = load_and_prep_dataset(BATCH_SIZE)
+    a = load_and_prep_dataset(BATCH_SIZE)
