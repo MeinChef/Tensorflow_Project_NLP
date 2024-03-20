fc570a3cc148f06c2f0b288df8f895b4bb5ecab4
MeinChef <yapa.nilop@gmail.com>
2024-03-20 21:52:32 +0100

    added full_25k.keras Tokeniser, implemented multi-GPU support

--- NLP-Wikidataset/main.py
+++ NLP-Wikidataset/main.py
@@ -7,7 +7,7 @@ from imports import os
 from imports import time
 import func
 
-tf.data.experimental.enable_debug_mode()
+# tf.data.experimental.enable_debug_mode()
 
 def from_gpt(batch_size, max_tokens = 50000):
     start = time.time()
@@ -63,64 +63,66 @@ if __name__ == '__main__':
 
     #### split data 
     #### remove last non -null element to unk token
-    ## maybe padding
-
-
-    EPOCHS = 16
-    BATCH_SIZE = 256 # 416 this is the highest value that works with 74154 (longest wikipedia)
-    BUFFER_SIZE = 1000
-    MAX_TOKENS = 50000 # to use pre-tokenised model use 50k, 75k, or 100k
-    PAD_SIZE = 45000
-
-
+    
+    # allows memory allocation, even if memory is not continuous
+    os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+    # some very interesting setup stuff
     tf.debugging.set_log_device_placement(True)
     gpus = tf.config.list_physical_devices('GPU')
+
 
     if gpus:
         print("Num GPUs Available: ", len(gpus))
         try:
-            strategy = tf.distribute.MirroredStrategy(gpus)
+            strategy = tf.distribute.MirroredStrategy()
             # Allocate 6.5 GB of memory to each GPU
             for gpu in gpus:
-                tf.config.set_logical_device_configuration(gpu, tf.config.LogicalDeviceConfiguration(memory_limit = 6656))
+                tf.config.set_logical_device_configuration(gpu, [tf.config.LogicalDeviceConfiguration(memory_limit = 6656)])
             logical_gpus = tf.config.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             # Virtual devices must be set before GPUs have been initialised
             print(e)
 
-    # do some setup stuff
-    if tf.__version__ != '2.15.0': print('You are not using Version 2.15.0 of Tensorflow, I haven\'t tested it with other versions, you are on your own :)')
-    # allows memory allocation, even if memory is not continuous
-    os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+    if tf.__version__ != '2.15.0': print('You are not using Version 2.15.0 of Tensorflow. The pretokenised models are not avaliable for you :)')
+
     # ensures that the working directory is set correctly, to work with Tokeniser.save_to_file() and LSTM.save_to_file()
     func.check_cwd()
 
-    # here the actual fun starts
+
+
+
+    # here the actual program starts: setting Hyperparameters
+    EPOCHS = 16
+    BATCH_SIZE = 150 # 416 this is the highest value that works with 74154 (longest wikipedia) and embedding of 1
+    BUFFER_SIZE = 1000
+    MAX_TOKENS = 25000 # to use pre-tokenised model use 25k, 50k, 75k, or 100k
+    PAD_SIZE = 20000
+
+    # timer
     start = time.time()
 
+    # get the data for the vocabulary
     raw_data = func.get_data(buff_size = BUFFER_SIZE, batch_size = BATCH_SIZE)
     tokeniser = Tokeniser(max_tokens = MAX_TOKENS)
     func.timer(start)
 
-    # for Mini11e trials
-    # raw_data = raw_data.take(20)
-    # list = raw_data.map(lamda x: whatever you do with x)
-
     ####### code to get tokens ######
     # with tf.device('/device:GPU0'):
-    #     tokeniser.adapt(raw_data)
+    # tokeniser.adapt(raw_data)
     # tokeniser.builder()
     # tokeniser.save_to_file(f'full_{int(MAX_TOKENS/1000)}k.keras')
 
     ###### code to load pre-tokenised model ######
     tokeniser.load_from_file(f'full_{int(MAX_TOKENS/1000)}k.keras')
+    
     func.timer(start)
     
     num_data, targets = func.targenise(raw_data, tokeniser, PAD_SIZE, pad_val = 0)
     del raw_data
     func.timer(start)
 
+    #### how we got the longest entry of the datset ####
     # longest = 0
     # for elem in num_data:
     #     if elem.shape[1] > longest: longest = elem.shape[1]
@@ -129,9 +131,10 @@ if __name__ == '__main__':
 
     
 
-    model = LSTM(layer_units = [5], embed_size = PAD_SIZE, max_tokens = MAX_TOKENS, output_dim = 50)
+    model = LSTM(layer_units = [64, 64], embed_size = PAD_SIZE, max_tokens = MAX_TOKENS, output_dim = 20)
     model.lazy_setter()
     model.info()
+    tf.print()
 
     model.train_test(data = num_data, targets = targets, epochs = EPOCHS)
 
